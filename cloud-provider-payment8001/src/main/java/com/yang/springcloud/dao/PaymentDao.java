bf833af859d9e5f98c30b364173eb77ad3f24401
yangmeng <ym652324@163.com>
2021-09-20 16:39:06 +0800

    完成简单的支付模块，创建订单模块

---
+++ cloud-provider-payment8001/src/main/java/com/yang/springcloud/dao/PaymentDao.java
@@ -0,0 +1,12 @@
+package com.yang.springcloud.dao;
+
+import com.yang.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface PaymentDao {
+    public int create(Payment payment);
+
+    public Payment getPaymentById(@Param("id") Long id);
+}
