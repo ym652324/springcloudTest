19bc0110c9ea6a9311e49eb1d31bb23eceef3883
yangmeng <ym652324@163.com>
2021-09-21 14:25:13 +0800

    完成简单的支付模块，订单模块，增加公共模块服务

--- cloud-provider-payment8001/src/main/java/com/yang/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/yang/springcloud/controller/PaymentController.java
@@ -5,10 +5,7 @@ import com.yang.springcloud.entities.CommonResult;
 import com.yang.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -16,8 +13,8 @@ public class PaymentController {
     @Autowired
     private PaymentService paymentService;
 
-    @PostMapping(value = "/payment/create",produces = "application/json;charset=utf-8")
-    public CommonResult create(Payment payment){
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(@RequestBody Payment payment){
         int result = paymentService.create(payment);
         log.info("******插入结果："+result);
         if(result>0){
